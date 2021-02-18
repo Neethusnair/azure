5dd6ceaed2a524f976f627b00af0aa62715cb7aa
Neethu <neethu.somasekharan@sreyo.com>
2021-02-18 14:47:48 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  subscription_id = "********"
+  client_id       = "*******"
+  client_secret   = "*******"
+  tenant_id       = "********"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "resource-1"
+  location = "westus"
+  tags = {
+    environment = "dev"
+    owner       = "neethu"
+  }
+}
